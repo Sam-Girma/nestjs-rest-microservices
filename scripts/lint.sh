707596040e3eb2982ca19467855f4fdc6f951a14
Samuel Girma Megra <samigirma53@gmail.com>
2024-10-18 12:00:00 +0300

    updated scripts

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,3 +4,4 @@ cd api-gateway && npm run lint && cd -
 cd microservices/comments-svc && npm run lint && cd -
 cd microservices/organizations-svc && npm run lint && cd -
 cd microservices/users-svc && npm run lint && cd -
+
